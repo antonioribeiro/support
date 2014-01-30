ba9a11bff6b0605d7bcbec2f4aa10ae75e28b33f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-01-30 17:10:12 -0200

    moving support classes from other packages

---
+++ src/CacheManager.php
@@ -0,0 +1,25 @@
+<?php namespace PragmaRX\Support;
+/**
+ * Part of the Support package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Support
+ * @version    1.0.0
+ * @author     Antonio Carlos Ribeiro @ PragmaRX
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2013, PragmaRX
+ * @link       http://pragmarx.com
+ */
+
+use Illuminate\Cache\CacheManager as IlluminateCacheManager;
+
+class CacheManager extends IlluminateCacheManager {
+
+}
