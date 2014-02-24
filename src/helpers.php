9b5a3c4acde2d5adce6857d8dc65dcbe27f6db02
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-02-24 20:31:08 -0300

    adding array_get helper

--- src/helpers.php
+++ src/helpers.php
@@ -208,3 +208,25 @@ if ( ! function_exists('explodeTree'))
 	    return $returnArr;
 	}
 }
+
+if ( ! function_exists('array_get'))
+{
+	private function array_get($array, $key, $default = null)
+	{
+		if (is_null($key)) return $array;
+
+		if (isset($array[$key])) return $array[$key];
+
+		foreach (explode('.', $key) as $segment)
+		{
+			if ( ! is_array($array) || ! array_key_exists($segment, $array))
+			{
+				return value($default);
+			}
+
+			$array = $array[$segment];
+		}
+
+		return $array;
+	}	
+}
