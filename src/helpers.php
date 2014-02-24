4184a6ed7db910ab532009e6a47ecb7c2034604e
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-02-24 16:42:15 -0300

    adding slash and contains helpers

--- src/helpers.php
+++ src/helpers.php
@@ -2,46 +2,90 @@
 
 use PragmaRX\Support\Exceptions\EnvironmentVariableNotSet;
 
-function env($variable)
+if ( ! function_exists('env'))
 {
-	$value = getenv($variable);
+	function env($variable)
+	{
+		$value = getenv($variable);
 
-	if ($value == false)
-	{
-		throw new EnvironmentVariableNotSet("Environment variable not set: $variable");
+		if ($value == false)
+		{
+			throw new EnvironmentVariableNotSet("Environment variable not set: $variable");
+		}
+
+		if ($value === '(false)')
+		{
+			$value = false;
+		}
+		else
+		if ($value === '(null)')
+		{
+			$value = null;
+		}
+		else
+		if ($value === '(empty)')
+		{
+			$value = '';
+		}
+
+		return $value;
 	}
 
-	if ($value === '(false)')
-	{
-		$value = false;
-	}
-	else
-	if ($value === '(null)')
-	{
-		$value = null;
-	}
-	else
-	if ($value === '(empty)')
-	{
-		$value = '';
-	}
-
-	return $value;
 }
 
-function getExecutablePath($cmd) 
+if ( ! function_exists('getExecutablePath'))
 {
-    return trim(shell_exec("which $cmd"));
+	function getExecutablePath($cmd) 
+	{
+	    return trim(shell_exec("which $cmd"));
+	}
 }
 
-function commandExists($cmd) 
+if ( ! function_exists('commandExists'))
 {
-    $returnVal = trim(getExecutablePath("which $cmd"));
-    
-    return (empty($returnVal) ? false : true);
+	function commandExists($cmd) 
+	{
+	    $returnVal = trim(getExecutablePath("which $cmd"));
+	    
+	    return (empty($returnVal) ? false : true);
+	}
 }
 
-function removeTrailingSlash($string)
+if ( ! function_exists('removeTrailingSlash'))
 {
-	return substr($string, -1) == '/' ? substr($string, 0, -1) : $string;
+	function removeTrailingSlash($string)
+	{
+		return substr($string, -1) == '/' ? substr($string, 0, -1) : $string;
+	}
+}
+
+if ( ! function_exists('str_contains'))
+{
+	function str_contains($haystack, $needles)
+	{
+		foreach ((array) $needles as $needle)
+		{
+			if ($needle != '' && strpos($haystack, $needle) !== false) return true;
+		}
+
+		return false;
+	}
+}
+
+if ( ! function_exists('is_windows'))
+{
+	function is_windows()
+	{
+		return str_contains(strtolower(php_uname()), 'windows');
+	}
+}
+
+if ( ! function_exists('slash'))
+{
+	function slash()
+	{
+		return is_windows()
+				? '\\'
+				: '/';
+	}
 }
