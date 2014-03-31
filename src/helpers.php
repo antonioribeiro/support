9504251eeb2ec25898b71cfe365305ead9526c1f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2014-03-30 22:58:58 -0300

    add a uasort comparer maker helper

--- src/helpers.php
+++ src/helpers.php
@@ -294,3 +294,48 @@ if ( ! function_exists('zz'))
 		die;
 	}
 }
+
+/**
+ * For usage check
+ *
+ *      http://stackoverflow.com/questions/96759/how-do-i-sort-a-multidimensional-array-in-php
+ *
+ */
+if ( ! function_exists('make_comparer')) {
+	function make_comparer()
+	{
+		// Normalize criteria up front so that the comparer finds everything tidy
+		$criteria = func_get_args();
+		foreach ($criteria as $index => $criterion) {
+			$criteria[$index] = is_array($criterion)
+				? array_pad($criterion, 3, null)
+				: array($criterion, SORT_ASC, null);
+		}
+
+		return function ($first, $second) use (&$criteria) {
+			foreach ($criteria as $criterion) {
+				// How will we compare this round?
+				list($column, $sortOrder, $projection) = $criterion;
+				$sortOrder = $sortOrder === SORT_DESC ? -1 : 1;
+
+				// If a projection was defined project the values now
+				if ($projection) {
+					$lhs = call_user_func($projection, $first[$column]);
+					$rhs = call_user_func($projection, $second[$column]);
+				} else {
+					$lhs = $first[$column];
+					$rhs = $second[$column];
+				}
+
+				// Do the actual comparison; do not return if equal
+				if ($lhs < $rhs) {
+					return -1 * $sortOrder;
+				} else if ($lhs > $rhs) {
+					return 1 * $sortOrder;
+				}
+			}
+
+			return 0; // tiebreakers exhausted, so $first == $second
+		};
+	}
+}
