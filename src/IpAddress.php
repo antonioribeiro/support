aa848b533edee7ad37e728d7df67e07af58342f8
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-12-24 23:42:05 +0000

    Check if cdir is valid

--- src/IpAddress.php
+++ src/IpAddress.php
@@ -221,10 +221,18 @@ class IpAddress {
 	 */
 	public static function cidrToRange($cidr)
 	{
+	    if (! preg_match("/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(\/[0-9]{1,2})?$/", $cidr)) {
+	        return false;
+        }
+
 		$range = array();
+
 		$cidr = explode('/', $cidr);
+
 		$range[0] = long2ip((ip2long($cidr[0])) & ((-1 << (32 - (int)$cidr[1]))));
+
 		$range[1] = long2ip((ip2long($cidr[0])) + pow(2, (32 - (int)$cidr[1])) - 1);
+
 		return $range;
 	}
 
