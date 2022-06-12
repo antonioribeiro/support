9b7f0fecf18be8a983c1f4539f76f8b77747984a
Arjen van Bochoven <arjen@bochoven.net>
2022-06-12 10:16:45 +0200

    Update helpers.php
    Make the value() function compatible with Laravel 8 as can be seen here:
    
    https://github.com/laravel/framework/blob/8.x/src/Illuminate/Collections/helpers.php#L175-L186
    
    This would fix some weird issues with Laravel when using the firewall library

--- src/helpers.php
+++ src/helpers.php
@@ -217,18 +217,17 @@ if ( ! function_exists('array_get') && ! function_exists('app'))
 	}
 }
 
-if ( ! function_exists('value'))
-{
-	/**
-	 * Return the default value of the given value.
-	 *
-	 * @param  mixed  $value
-* @return mixed
-	 */
-	function value($value)
-	{
-		return $value instanceof Closure ? $value() : $value;
-	}
+if (! function_exists('value')) {
+    /**
+     * Return the default value of the given value.
+     *
+     * @param  mixed  $value
+     * @return mixed
+     */
+    function value($value, ...$args)
+    {
+        return $value instanceof Closure ? $value(...$args) : $value;
+    }
 }
 
 if ( ! function_exists('array_pluck'))
